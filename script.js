9143965c3729b0bc2ecc2fa4e083350a6472572a
Nestan <nestankeneshova16@gmail.com>
2023-02-21 14:50:10 +0600

    changed paragraph 4

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ console.log(example2);
 
 let example3 = document.querySelector(".example-3");
 console.log(example3);
+
+let example4 = document.querySelector("#example-4");
+console.log(example4);
+
+let example5 = document.querySelector("p:nth-child(4");
+console.log(example5);
