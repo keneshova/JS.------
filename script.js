b3e300a1e161064639c69f08dbd7f9ae791638c4
Nestan <nestankeneshova16@gmail.com>
2023-02-21 16:07:05 +0600

    added a lot of things and stylized

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let example2 = document.querySelector("p");
 example2.textContent = "Hello world";
 example2.style.color  = "tomato";
 example2.style.fontSize = "60px";
+example2.style.textAlign = "center";
 example2.style.backgroundColor = "pink";
 console.log(example2);
 
@@ -23,3 +24,23 @@ for (let i = 0; i < example6.length; i++) {
   let paragraph = example6[i];
   paragraph.textContent += " 🌹";
 }
+
+let example7 = document.createElement("p");//соз пусьой параг 
+example7.textContent = "Lorem ispun";//доб лорем
+//document.body.prepend(example7);//прив-м к боди доб-т в конце этого эл
+
+example1.append(example7);
+
+//сщз код кот хранит пустой тег ul
+
+let example8 = document.createElement("ul");
+example8.className = "example-8";
+document.body.append(example8);
+for( let i = 1; i <= 10; i++){
+  let listItem = document.createElement("li");
+  listItem.textContent = "Example " + i;
+  listItem.style.color = "green";
+  example8.append(listItem);
+}
+
+
